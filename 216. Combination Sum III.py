e2e9e41d0facb3d32e1aaf99cea1846dcc729daf
Yee <cittie.yi@gmail.com>
2016-12-02 15:28:14 +0800

    216

---
+++ 216. Combination Sum III.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+
+        result = []
+
+        def search(num = 0, nums = []):
+            count = len(nums)
+            total = sum(nums)
+
+            if count > k or total > n:
+                return
+
+            if count == k and total == n:
+                result.append(nums)
+                return
+
+            for i in range(num + 1, 10):
+                search(i, nums + [i] )
+
+        search()
+
+        return result
+        
