06825ce5a30adf4f577f8b92c9676b2972bd7730
Yee <cittie.yi@gmail.com>
2016-12-21 18:24:10 +0800

    445

---
+++ 445. Add Two Numbers II.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+        stack1, stack2= [], []
+        last = None
+        plusOne = 0
+
+        while l1:
+            stack1.append(l1)
+            l1 = l1.next
+
+        while l2:
+            stack2.append(l2)
+            l2 = l2.next
+
+        while stack1 or stack2:
+
+            val1 = stack1.pop().val if stack1 else 0
+            val2 = stack2.pop().val if stack2 else 0
+            total = val1 + val2 + plusOne
+
+            if total > 9:
+                plusOne = 1
+                node = ListNode(total - 10)
+            else:
+                plusOne = 0
+                node = ListNode(total)
+
+            node.next = last
+            last = node
+
+        # Additional situation
+        if plusOne:
+            node = ListNode(1)
+            node.next = last
+            last = node
+
+        return last
