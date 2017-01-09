1900a7ebe2738c73be022ea66230ce16aac98169
Yee <cittie.yi@gmail.com>
2017-01-09 16:08:47 +0800

    393
    Improve performance.

--- 393. UTF-8 Validation.py
+++ 393. UTF-8 Validation.py
@@ -5,13 +5,14 @@ class Solution(object):
         :rtype: bool
         """
 
-        size = len(data)
+        i, size = 0, len(data)
+
 
         head_mask = 0b10000000
 
-        while data:
-            head = data.pop(0)
-            size -= 1
+        while i < size:
+            head = data[i]
+            i += 1
 
             if head >> 7:      # Check if head staring with 1
                 head <<= 1       # Drop the first
@@ -22,15 +23,14 @@ class Solution(object):
                 if n < 1 or n > size:   # Check if byte count is available
                     return False
 
-                count = 0               # Count the following n elements head with '0b10'.
-                for i in range(n):
+                # Count the following n elements head with '0b10'.
+                for _ in range(n):
                     if data[i] >> 6 == 0b10:
-                        count += 1
+                        i += 1
+                        n -= 1
 
-                if count != n:
+                if n:
                     return False
-                else:
-                    data = data[n:]     # Step forward
         else:
             return True
             
