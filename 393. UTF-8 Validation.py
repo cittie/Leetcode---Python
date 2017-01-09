9eab577386646395f7dea7c97be4c843b9270c80
Yee <cittie.yi@gmail.com>
2017-01-09 16:20:42 +0800

    393
    Improve code readability

--- 393. UTF-8 Validation.py
+++ 393. UTF-8 Validation.py
@@ -5,32 +5,29 @@ class Solution(object):
         :rtype: bool
         """
 
-        i, size = 0, len(data)
-
-
+        index, size = 0, len(data)
         head_mask = 0b10000000
 
-        while i < size:
-            head = data[i]
-            i += 1
+        while index < size:
+            head = data[index]
+            index += 1
 
             if head >> 7:      # Check if head staring with 1
                 head <<= 1       # Drop the first
-                n = 0
+                count = 0
                 while head & head_mask:     # Check how many following bytes followed head
-                    n += 1
+                    count += 1
                     head <<= 1
-                if n < 1 or n > size:   # Check if byte count is available
+                if count < 1 or count > size:   # Check if byte count is available
                     return False
 
                 # Count the following n elements head with '0b10'.
-                for _ in range(n):
-                    if data[i] >> 6 == 0b10:
-                        i += 1
-                        n -= 1
+                for _ in range(count):
+                    if data[index] >> 6 == 0b10:
+                        index += 1
+                        count -= 1
 
-                if n:
+                if count:
                     return False
         else:
             return True
-            
